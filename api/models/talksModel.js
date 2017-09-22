861c15a46f1de0b5e2be4d45dc3e8adbb16dff1f
brain <nvtam2105@gmail.com>
2017-09-22 16:37:25 +0700

    Merge talk and talk detail and save to db

--- api/models/talksModel.js
+++ api/models/talksModel.js
@@ -2,16 +2,6 @@
 'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-// "id": 2863,
-// "event_id": 455,
-// "name": "Laolu Senbanjo: \"The Sacred Art of the Ori\"",
-// "description": "Every artist has a name, and every artist has a story. Laolu Senbanjo's story started in Nigeria, where he was surrounded by the culture and mythology of the Yoruba, and brought him to law school, to New York and eventually to work on Beyoncé's \"Lemonade.\" He shares what he calls \"The Sacred Art of the Ori,\" art that uses skin as canvas and connects artist and muse through mind, body and soul.",
-// "slug": "laolu_senbanjo_the_sacred_art_of_the_ori",
-// "native_language_code": "en",
-// "published_at": "2017-08-25 14:57:57",
-// "recorded_at": "2017-04-24 00:00:00",
-// "updated_at": "2017-09-14 10:21:40",
-// "released_at": "2017-08-25 14:58:04"
 
 var TalksSchema = new Schema({
   id: {type: Number},
@@ -23,13 +13,12 @@ var TalksSchema = new Schema({
   published_at: {type: Date},
   recorded_at: {type: Date},
   updated_at: {type: Date},
-  //released_at: {type: Date},
   viewed_count: {type: Number},
   images: [{size: String, url: String}],
-  medias: [{size: String, url: String, size: Number, mime_type: String}],
-  langs: [{code: String,name: String}],
   speakers: [{name: String}],
-  tags: [{name: String}]
+  tags: [{name: String}],
+  medias: [{name: String, url: String, size: Number, mime_type: String}],
+  langs: [{code: String, name: String}]
 });
 
 module.exports = mongoose.model('Talks', TalksSchema);
