e1d1606982b3f9812c5b720cd12511dcecbd9940
brain <nvtam2105@gmail.com>
2017-10-17 18:08:46 +0700

    update controller script

--- api/controllers/scriptsController.js
+++ api/controllers/scriptsController.js
@@ -1,12 +1,17 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-Scripts = mongoose.model('Scripts');
+  Scripts = mongoose.model('Scripts');
 
-exports.get_scripts_of_talk = function(req, res) {
-  Scripts.findOne({ 'talk_id': req.params.talkId }, function(err, script) {
-    if (err)
-      res.send(err);
-    res.json(script);
-  });
+exports.get_scripts_of_talk = function (req, res) {
+  Scripts.findOne({ 'talk_id': req.params.talkId }).
+    select({
+      '_id': 0,
+      '__v': 0
+    }).
+    exec(function (err, script) {
+      if (err)
+        res.send(err);
+      res.json(script);
+    });
 };
